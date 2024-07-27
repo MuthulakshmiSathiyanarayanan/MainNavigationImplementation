dd848db5a6edb37a5c9216829dc3e2ce995dd4e1
muthulakshmi <muthulakshmisathiyanarayanan5@gmail.com>
2024-07-27 23:02:53 +0530

    main-nav-bar

---
+++ app/pages/electronics/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      ELECTRONICS PAGE
+    </div>
+  )
+}
+
+export default page
---
+++ app/pages/womensClothing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      womensClothing
+    </div>
+  )
+}
+
+export default page
