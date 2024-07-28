bfbf8ff4e119c92334e22cf0fe5513a2d2b66424
muthulakshmi <muthulakshmisathiyanarayanan5@gmail.com>
2024-07-28 07:59:56 +0530

    tailwind  issues fixed

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,28 @@ export default function Home() {
       />
     <nav className="bg-[#c9e3fb] p-4 top-0 left-0 w-full  z-[1000]">
       <ul className="list-none flex justify-around p-0 m-0">
-        <li>
+        <li className="m-0">
           <Link href="/">Home</Link>
         </li>
-        <li>
-          <Link href="/pages/electronics">Electronics</Link>
+        <li className="m-0">
+          <Link href="/pages/electronics"legacyBehavior>
+          <a className="text-gray-900 no-underline px-4 py-2 transition-colors duration-300 hover:bg-gray-200">Electronics</a></Link>
         </li>
-        <li>
-          <Link href="/pages/jewelery">Jewelery</Link>
+        <li className="m-0">
+            <Link href="/pages/jewelery" legacyBehavior>
+          <a className="text-gray-900 no-underline px-4 py-2 transition-colors duration-300 hover:bg-gray-200">
+            Jewelery
+          </a>
+        </Link>
         </li>
-        <li>
-          <Link href="/pages/mensClothing">Men'sClothing</Link>
+        <li className="m-0">
+          <Link href="/pages/mensClothing"legacyBehavior>
+          <a className="text-gray-900 no-underline px-4 py-2 transition-colors duration-300 hover:bg-gray-200">Men'sClothing</a></Link>
           </li>
-          <li>
-            <Link href="/pages/womensClothing">Women'sClothing</Link>
+          <li className="m-0">
+            <Link href="/pages/womensClothing" legacyBehavior>
+              <a className=" px-4 py-2 hover:bg-custom-gray transition-colors duration-300">Women'sClothing</a>
+              </Link>
           </li>
       </ul>
     </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+       colors: {
+      "custom-gray": "#9ca49f", // Add your custom color here
     },
   },
+},
   plugins: [],
 };
 export default config;
