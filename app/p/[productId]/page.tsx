680b63a7261dc4103923b74abfcd0ccc1bc01f37
muthulakshmi <muthulakshmisathiyanarayanan5@gmail.com>
2024-08-02 10:34:45 +0530

    reverting change

--- app/p/[productId]/page.tsx
+++ app/p/[productId]/page.tsx
@@ -1,6 +1,9 @@
 // app/c/[category]/page.tsx (or .js)
-import { Product } from '@/app/components/type'; // Ensure this import path is correct
-import ProductDetail from './productDetail';
+import Image from 'next/image';
+import { Product } from '@/app/components/type';
+
+
+
 // Function to fetch products by category
 async function fetchProducts(productId:number) {
   const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
@@ -10,7 +13,7 @@ async function fetchProducts(productId:number) {
     throw new Error('Failed to fetch products');
   }
   // console.log(data);
-  return [data];
+  return data;
  
 }
 // Server-side component for the category page
@@ -24,9 +27,27 @@ export default async function ProductPage({ params }: { params: { productId:numb
     }
     return (
       <div>
-        {products?.map((e) => (
-          <ProductDetail{...e} />
-       ))}
+        <ul className="flex flex-col bg-[#e9e4ad]">
+          {/* {products.map((product) => ( */}
+            <li
+              className="bg-[#cbd8e0] p-1 rounded-3xl mt-6"
+            
+            >
+              <h1 className="flex  ml-[25px]">ID: {products.id}</h1>
+              <Image
+                className="p-[5px] max-w-[150px] min-w-[150px] max-h-[180px] min-h-[180px] bg-customDark rounded-[20px] mt-[15px] ml-[25px]"
+                src={products.image}
+                alt={products.title}
+                width={180}
+                height={180}
+              />
+              <p className="flex ml-[25px]">${products.price}</p>
+              <h2 className="bg-customLight italic text-lg font-normal rounded-[5px] p-[2px] ml-[25px] break-words flex flex-wrap justify-center max-w-[150px]">
+                {products.title}
+              </h2>
+            </li>
+           {/* ))} */}
+        </ul>
       </div>
     );
     
